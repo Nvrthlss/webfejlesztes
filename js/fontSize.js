05a65fae2bcda1c0540f65becc7034cee0d76c72
Nvrthlss <nvrthlsss@gmail.com>
2025-05-11 19:12:24 +0200

    Add files via upload

--- js/fontSize.js
+++ js/fontSize.js
@@ -1,6 +1,7 @@
 const fontSizeButton = document.getElementById("betumeretGomb");
 
 let isLarge = false;
+let isHighContrast = false;
 
 fontSizeButton.addEventListener("click", function() {
     if (!isLarge) {
@@ -9,5 +10,12 @@ fontSizeButton.addEventListener("click", function() {
         document.documentElement.style.fontSize = "100%";
     }
 
+    if (!isHighContrast) {
+        document.body.classList.add("high-contrast");
+    } else {
+        document.body.classList.remove("high-contrast");
+    }
+
     isLarge = !isLarge;
+    isHighContrast = !isHighContrast;
 });
